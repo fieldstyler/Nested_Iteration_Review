b28b7314fd8a3314b74fa7633400b1c0979b77e7
fieldstyler <tdfields93@gmail.com>
2022-06-02 10:36:37 -0600

    Update warm_up.rb

--- encanto/warm_up.rb
+++ encanto/warm_up.rb
@@ -4,11 +4,3 @@ cousins = [["Isabella", "Luisa", "Mirabel"], ["Dolores", "Camilo", "Antonio"]]
 # 2. What is the data type of cousins[1]? cousins[1][1]?
 # 3. What command will return 'Dolores?'
 # 4. What command(s) will return the integer 3?
-
-# 1. What is cousins.size?
-# 2. What is cousins[1][2].length?
-# 3. What is the data type of cousins[1]? cousins[1][1]?
-# 4. What command will return ["Isabella", "Luisa", "Mirabel"]?
-# 5. What command will return 'Dolores?'
-# 6. What command(s) will return the integer 3?
-# 7. What is the value of cousins[2]? Why?
